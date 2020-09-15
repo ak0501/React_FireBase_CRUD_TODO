1ef8748533d8cdfde76f3f21ecbc35282711905e
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-09-15 05:54:42 -0500

    initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,25 @@
+import * as firebase from "firebase";
+import 'firebase/auth';
+import 'firebase/app';
+import 'firebase/database';
+import 'firebase/firestore';
+
+
+let firebaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+});
+// Initialize Firebase
+
+firebase.analytics();
+// firebase.auth();
+ const db=firebaseApp.firestore();
+
+export default db;
+
