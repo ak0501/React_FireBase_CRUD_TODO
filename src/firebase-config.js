678792bf02fdfed3d2b1717911ffd354fcf52456
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-09-15 11:34:51 -0500

    test API

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,8 @@
 import * as firebase from "firebase";
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 let firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey:process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   databaseURL: process.env.REACT_APP_DATABASEURL,
   projectId: process.env.REACT_APP_PROJECT_ID,
